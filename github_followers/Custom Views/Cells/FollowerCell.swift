bb778e1df948659d3e788f2f2bb6e8b0000208e0
Bence Fulop <bence.fulop@arq.group>
2022-03-17 16:11:33 +1100

    create tableviewcell with formatting

---
+++ github_followers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,46 @@
+//
+//  FollowerCell.swift
+//  github_followers
+//
+//  Created by Bence Fulop on 17/3/22.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "followerCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    private let padding: CGFloat = 8
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ github_followers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  github_followers
+//
+//  Created by Bence Fulop on 17/3/22.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage!
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
