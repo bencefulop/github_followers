eec7c2cb6fff6284a1484aeb58ddd397aaa057b5
Bence Fulop <bence.fulop@arq.group>
2022-04-08 13:59:43 +1000

    add loading indicators

--- github_followers/Extensions/UIViewController+Extension.swift
+++ github_followers/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+private var containerView: UIView!
+
 extension UIViewController {
 
     func presentGFAlertOnMainThread(alertTitle: String, alertMessage: String, buttonTitle: String) {
@@ -15,6 +17,31 @@ extension UIViewController {
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        UIView.animate(withDuration: 0.5) { containerView.alpha = 0.8 }
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
